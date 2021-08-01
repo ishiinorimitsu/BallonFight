1ab1b0d683997f30fa53f4aa02048ddcef86a9de
ishiinorimitsu <shijingqianguang619@gmail.com>
2021-08-01 18:50:20 +0900

    手順20まで完了

---
+++ Assets/Scripts/GoalChecker.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalChecker : MonoBehaviour
+{
+    public float moveSpeed = 0.01f;
+    private float stopPos = 6.5f;
+    private bool isGoal;
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x > stopPos)
+        {
+            transform.position += new Vector3(-moveSpeed,0,0);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player" && isGoal == false)
+        {
+            isGoal = true;
+            Debug.Log("ÉQÅ[ÉÄÉNÉäÉA");
+        }
+    }
+}
