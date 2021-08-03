e157422dc5f6481d8bbefbfac01c4d5120fa85cc
ishiinorimitsu <shijingqianguang619@gmail.com>
2021-08-03 21:32:22 +0900

    手順22完了

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
@@ -21,6 +22,9 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private Transform canvasTran;
 
+    [SerializeField]
+    private Button btnInfo;
+
     ///<param name = "score" ></ param>
     public void UpdateDisplayScore(int score)
     {
@@ -31,11 +35,24 @@ public class UIManager : MonoBehaviour
     {
         canvasGroupInfo.DOFade(1.0f,1.0f);
         txtInfo.DOText("Game Over...", 1.0f);
+        btnInfo.onClick.AddListener(RestartGame);
     }
 
     public void GenerateResultPopUp(int score)
     {
         ResultPopUp resultPopUp = Instantiate(resultPopUpPrefab, canvasTran, false);
         resultPopUp.SetUpResultPopUp(score);
+    }
+
+    public void RestartGame()
+    {
+        btnInfo.onClick.RemoveAllListeners();
+        string sceneName = SceneManager.GetActiveScene().name;
+        canvasGroupInfo.DOFade(0f, 1.0f)
+            .OnComplete(() =>
+             {
+                 Debug.Log("Restart");
+                 SceneManager.LoadScene(sceneName);
+             });
     }
 }
